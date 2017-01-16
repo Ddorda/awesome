cfaa278630c654e36aeb71cfe129f8a119141dba
Ddorda <ddorda@ubuntu.com>
2017-01-16 02:33:39 +0200

    replace io.popen with run function

--- widgets/kbswitch.lua
+++ widgets/kbswitch.lua
@@ -6,13 +6,13 @@ kbswitch.primary_lang = 'us'
 
 kbswitch.switch = function ()
   local current_kb
-  current_kb = io.popen("xkb-switch -p -n"):read("*l")
+  current_kb = run("xkb-switch -p -n")
   kbswitch.widget:set_text(" " .. current_kb .. " ")
 end
 kbswitch.set_layout = function(layout)
   local current_kb
-  io.popen("xkb-switch -s " .. layout)
-  current_kb = io.popen("xkb-switch -p"):read("*l")
+  run("xkb-switch -s " .. layout)
+  current_kb = run("xkb-switch -p")
   kbswitch.widget:set_text(" " .. current_kb .. " ")
   if (current_kb == layout) then
     return true 
